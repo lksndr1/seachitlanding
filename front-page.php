92d22511d370b45c35358eb0eb9905601f9bad6a
hettfield git <olekpl90@gmail.com>
2024-11-21 11:51:12 +0100

    second section

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
     <section class='section hero-section'>
         <div class='container'>
             <div class='hero-section__content'>
-                <p class='hero-section__text'>Embark on a magical adventure</p>
+                <h1 class='hero-section__text'>Embark on a magical adventure</h1>
                 <div class='hero-section__buttons'>
                     <button class='hero-section__buttons__calculator'>Loan calculator</button>
                     <button class='hero-section__buttons__read'>Read more</button>
@@ -25,7 +25,18 @@
 <main>
     <section class="section home-section">
         <div class="container">
-            home
+            <div class="home-section__flex-wrapper">
+                <?php 
+                    $image = get_field('home_section_image');
+                    if( !empty( $image ) ): ?>
+                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                <?php endif; ?>
+                <div class="home-section__flex-wrapper__description">
+                    <h5>Discover the Magic of Maxter cluster Pro</h5>
+                    <p>You will be taken to a magical world of online gaming. With its launch in 2023, a new era of adventures and emotions began. From the legendary Big Cluster of Scary to the treasure of Extra Monster Pro.</p>
+                    <button>Read more</button>
+                </div>
+            </div>
         </div>
     </section>
     <section class="section new-games-section">
